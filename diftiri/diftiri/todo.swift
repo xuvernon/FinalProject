7cdb8bce8a91dc8f8958b3c42778612a1ea7c7a2
sarah almutari <sara0moha78@gmail.com>
2022-03-28 17:29:23 +0300

    .

--- diftiri/diftiri/todo.swift
+++ diftiri/diftiri/todo.swift
@@ -8,51 +8,58 @@
 import SwiftUI
 struct todo: View {
     @State var note = ""
-
+    
+    @State var Task = false
        
     var body: some View {
+    
         VStack{
             TextField("Page Title", text: $note)
                 .frame(width: 250, height: 100, alignment: .center)
                 .ignoresSafeArea()
+
+            List{
+            HStack{
+            Image(systemName:"checkmark.square")
+                    .frame(width: 30, height: 30, alignment: .center)
+    .onTapGesture {
+               Task.toggle()
+            print(Task)
+                    }
+            TextField("task", text: $note)
+                    
+            .frame(width: 350, height: 50, alignment: .center)
+                    .ignoresSafeArea()
+            }
+           
+                
+            
+            HStack{
+            Image(systemName:"checkmark.square")
+                    .frame(width: 30, height: 30, alignment: .center)
+           
+            TextField("task", text: $note)
+                    
+            .frame(width: 350, height: 50, alignment: .center)
+                    .ignoresSafeArea()
+            }
+            HStack{
+            Image(systemName:"checkmark.square")
+                    .frame(width: 30, height: 30, alignment: .center)
+                
+            TextField("task", text: $note)
+                    
+            .frame(width: 350, height: 50, alignment: .center)
+                    .ignoresSafeArea()
+            }
             Spacer()
-        
-            HStack{
-            Image(systemName:"checkmark.square")
-                    .frame(width: 30, height: 30, alignment: .center)
-                
-            TextField("task", text: $note)
-                    
-            .frame(width: 350, height: 50, alignment: .center)
-                    .ignoresSafeArea()
             }
-          
-            HStack{
-            Image(systemName:"checkmark.square")
-                    .frame(width: 30, height: 30, alignment: .center)
-            .onTapGesture {
-//                if (){}
-            }
-                
-            TextField("task", text: $note)
-                    
-            .frame(width: 350, height: 50, alignment: .center)
-                    .ignoresSafeArea()
-            }
-            HStack{
-            Image(systemName:"checkmark.square")
-                    .frame(width: 30, height: 30, alignment: .center)
-                
-            TextField("task", text: $note)
-                    
-            .frame(width: 350, height: 50, alignment: .center)
-                    .ignoresSafeArea()
-            }
-            Spacer()
+        }
+
         }
          
         }
-    }
+
 
 struct todo_Previews: PreviewProvider {
     static var previews: some View {
